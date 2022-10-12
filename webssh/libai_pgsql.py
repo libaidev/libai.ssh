4d8abca830dbf31de4bab954b067836bc7343156
Charles(李传义) <lichuanyi@home.langchao.com>
2022-10-12 14:06:01 +0800

    add pgsql

--- webssh/libai_pgsql.py
+++ webssh/libai_pgsql.py
@@ -1,22 +1,44 @@
 # -* - coding: UTF-8 -* -
 # !/usr/bin/python3
 
+from typing import Optional, Awaitable
+
 import psycopg2
-#连接数据库
-conn = psycopg2.connect(database='test',user='postg',
-                 password='y0',host='10.13.108.5',port='5432')
+import tornado.web
 
-cursor = conn.cursor()   #创建一个游标
 
-#execuet方法是针对数据库的请求
-cursor.execute('create table test_1(id int,name varchar(20) )')
-cursor.execute("insert into test_1 values(1,'lg')")
-cursor.execute("drop table test_1")
+class PgsqlHandler(tornado.web.RequestHandler):
 
-conn.commit()  #提交
-cursor.execute("select * from test_1")
-rows = cursor.fetchall()   #返回所有行的元组
-for row in rows:
-    print(row[0],row[1])
-cursor.close()
-conn.close()
+    def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
+        pass
+
+    def get(self):
+        sql = self.get_argument("sql", default="select 1")
+        db = self.get_argument("db", default="10.110.62.41|root|Jya3QE7M0e|icp_native|30862")
+        # 10.110.62.41|root|Jya3QE7M0e|icp_native|30862
+        db_arr = db.split("|")
+        conn = psycopg2.connect(host=db_arr[0], user=db_arr[1], passwd=db_arr[2], db=db_arr[3], port=int(db_arr[4]))
+        self._exec_sql(conn, sql)
+
+    def _exec_sql(self, conn, sql):
+        try:
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            conn.commit()
+            result = cursor.fetchall()
+        finally:
+            cursor.close()
+            conn.close()
+
+        html = ""
+        for row in result:
+            for item in row:
+                html = html + str(item) + " | "
+            html = html + "\n"
+        self.write(html)
+
+    def post(self):
+        pass
+
+    def set_default_headers(self):
+        self.set_header('Content-type', 'application/json;charset=utf-8')
